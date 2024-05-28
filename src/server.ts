a415bea1b1aad20528cf09bd82e0d4842e279358
Jonathan Vanegas <jonathanvanegasg@hotmail.com>
2024-05-27 23:10:21 -0500

    modifcando swwager

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
 import { connectDB } from "./config/database";
 import cors from "cors";
 import swaggerUi from "swagger-ui-express";
@@ -21,24 +21,28 @@ class Server {
     notificationMatch: "/api/notificationMatch",
   };
 
+  private CSS_URL =
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
   constructor() {
     this.app = express();
     this.port = process.env.PORT || "3000";
 
-    // //Iniciar la base de datos
+    // Iniciar la base de datos
     connectDB();
 
-    //Métodos Iniciales
+    // Métodos Iniciales
     this.middlewares();
 
     // Rutas
     this.routes();
+
+    // Configurar la interfaz de Swagger-UI
+    this.setupSwaggerUI();
   }
 
   middlewares() {
     this.app.use(cors());
-
-    // Lectura del Body
     this.app.use(express.json());
   }
 
@@ -51,10 +55,13 @@ class Server {
     this.app.use(this.apiPaths.match, matchRouter);
     this.app.use(this.apiPaths.auth, loginRouter);
     this.app.use(this.apiPaths.notificationMatch, notificationRouter);
+  }
+
+  setupSwaggerUI(): void {
     this.app.use(
       this.apiPaths.talentoTechApi,
       swaggerUi.serve,
-      swaggerUi.setup(swaggerSpec)
+      swaggerUi.setup(swaggerSpec, { customCssUrl: this.CSS_URL })
     );
   }
 
