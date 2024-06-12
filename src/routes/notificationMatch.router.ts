f5029938ee8fe3455a46482dbe54e7dc692dad14
Jonathan Vanegas <jonathanvanegasg@hotmail.com>
2024-06-12 17:14:58 -0500

    modificando documentacion swagger

--- src/routes/notificationMatch.router.ts
+++ src/routes/notificationMatch.router.ts
@@ -120,6 +120,115 @@ export default router;
  *                    type: string
  *                    example: Error interno del servidor
  *
+ * /api/notificationMatch/notifications/user/{notificationId}/accepted:
+ *  put:
+ *    summary: aceptar una notificación por el usuario
+ *    tags: [Notifications Match]
+ *    security:
+ *     - bearerAuth: []
+ *    parameters:
+ *    - in: path
+ *      name: notificationId
+ *      required: true
+ *      description: ID de la notificación del match
+ *      schema:
+ *        type: string
+ *    responses:
+ *       200:
+ *         description: Notificación aceptada exitosamente
+ *         content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *                    example: Notificación aceptada exitosamente
+ *       400:
+ *         description: Error al intentar aceptar la Notificación
+ *         content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *                    example: Error al intentar aceptar la Notificación
+ *       401:
+ *         description: No se encontró el token de autenticación
+ *         content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *                    example: No se encontró el token de autenticación
+ *       500:
+ *         description: Error interno del servidor
+ *         content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *                    example: Error interno del servidor
+ * /api/notificationMatch/notifications/admin/{notificationId}/cancel:
+ *  put:
+ *    summary: cancelar una notificación por el administrador
+ *    tags: [Notifications Match]
+ *    security:
+ *     - bearerAuth: []
+ *    parameters:
+ *    - in: path
+ *      name: notificationId
+ *      required: true
+ *      description: ID de la notificación del match
+ *      schema:
+ *        type: string
+ *    responses:
+ *       200:
+ *         description: Notificación cancelada exitosamente
+ *         content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *                    example: Notificación cancelada exitosamente
+ *       400:
+ *         description: Error al intentar cancelar la Notificación
+ *         content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *                    example: Error al intentar cancelar la Notificación
+ *       401:
+ *         description: No se encontró el token de autenticación
+ *         content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *                    example: No se encontró el token de autenticación
+ *       500:
+ *         description: Error interno del servidor
+ *         content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *                    example: Error interno del servidor
+ *  
  *
  *
  */
